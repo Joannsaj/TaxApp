fdfa1ab2deac11e93b25be2ddf4d583a71f1f5b9
Fred <akamaatei@gmail.com>
2020-09-09 23:17:47 +0300

    modify deadline day

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ function closeNav() {
 }
 
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 11, 2020 15:37:25").getTime();
+var countDownDate = new Date("Sep 25, 2020 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
