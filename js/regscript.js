2b3f5ec2ad6111864db45e70e86aea55d178434a
bethwelkip <48167977+bethwelkip@users.noreply.github.com>
2020-09-10 11:23:52 -0400

    Bethwel (#14)
    * correct typos and styling
    
    * edit readme links
    
    * edit readme links
    
    * add tax form formulas
    
    * add function for final taxes
    
    * add jumbotron elements for sectioning
    
    * add section background colors
    
    * add header text colors
    
    Co-authored-by: Joannsaj <69344291+Joannsaj@users.noreply.github.com>

---
+++ js/regscript.js
@@ -0,0 +1,70 @@
+/* Residential Details*/
+
+
+
+/* Employees Profession*/
+
+
+
+
+/* National ID details*/
+
+
+
+
+
+
+
+/*Individual Data */
+
+
+
+
+
+/*Father */
+
+
+/*Mother */
+
+
+
+/* Spouse */
+
+
+
+/*Physical Address */
+
+
+
+/* Postal Address*/
+
+
+
+/* Contact Details*/
+
+
+
+/*SMS Notification */
+
+
+
+/* Alternative Address*/
+
+
+
+/* Bank Account */
+
+
+/*Business */
+
+
+
+/*Bonds */
+
+
+
+/* */
+
+
+
+/* */
